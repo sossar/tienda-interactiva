7182f1d1059c4aa5b6487b75851802afd694ccf4
sossar <sossar@unal.edu.co>
2025-05-05 12:49:33 -0500

    Cambios hechos en clase, java

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function carrito(){
     contador+=1
     document.getElementById("carrito").innerHTML = contador;
 }
+
+function init(){
+    var card = document.getElementById("card1")
+    card = card.innerHTML =  
+    `
+    <div>
+        <h1>Lista prod </h1> 
+        <h2 id="nombreproducto">Producto 1</h2>
+        <input type="number"  id="input1">
+        <button onclick="agregarCarrito()" type="button" class = "btn btn-success btn-sm">Añadir</button>
+
+    </div>
+    
+    
+    `
+}
+function agregarCarrito(){
+    var nombre = document.getElementById("nombreproducto")
+    var valor = document.getElementById("input1").value
+    var carrito = document.getElementById("carrito")
+    carrito = carrito.innerHTML = `<h1>valor: ${valor} y nombre: ${nombreproducto}</h1>`
+}
